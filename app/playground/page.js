9cfc934e1b5ce8ce5e2962f7a6d065c2ae81fdae
Luke Cheng <Lukeecheng@gmail.com>
2025-03-29 14:33:23 -0700

    configured button component

--- app/playground/page.js
+++ app/playground/page.js
@@ -2,6 +2,7 @@ import PageHeader from "@/components/page-header";
 import Trend from "@/components/trend";
 import TransactionItem from "@/components/transaction";
 import TransactionSummaryItem from "@/components/transaction-summary";
+import Button from "@/components/button";
 
 export default function Page() {
   return (
@@ -46,6 +47,20 @@ export default function Page() {
           <TransactionItem type="Expense" category="Food" description="Going out to eat" amount={29} />
           <TransactionItem type="Saving" description="For children" amount={500} />
           <TransactionItem type="Investment" description="In Microsoft" amount={9000} />
+        </div>
+      </div>
+
+      <div>
+        <h2 className="mb-4 text-lg font-mono">Buttons</h2>
+        <hr className="mb-4 border-gray-200 dark:border-blue-300 border-2" />
+        <div className="space-x-4">
+          <Button>Hello</Button>
+          <Button variant="outline">Hello</Button>
+          <Button variant="ghost">Hello</Button>
+
+          <Button size="xs">Hello</Button>
+          <Button size="sm">Hello</Button>
+          <Button size="lg">Hello</Button>
         </div>
       </div>
     </main>

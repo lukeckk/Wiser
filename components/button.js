6acdfa6479a096e64c6b05cf020276b2824f5825
Luke Cheng <Lukeecheng@gmail.com>
2025-03-31 13:42:15 -0700

    configured light and dark mode toggle

--- components/button.js
+++ components/button.js
@@ -1,4 +1,4 @@
-export default function Button(props) {
+export default function Button({ children, variant, size, ...props }) {
   const variants = {
     default: 'bg-black text-white dark:bg-white dark:text-black rounded-md hover:bg-gray-700 dark:hover:bg-gray-200',
     outline: 'border border-gray-300 dark:border-gray-500 rounded-md hover:bg-gray-300 dark:hover:bg-gray-500',
@@ -10,5 +10,12 @@ export default function Button(props) {
     base: 'text-base px-4 py-2',
     lg: 'text-lg px-4 py-2'
   }
-  return (<button {...props} className={`${props.variant ? variants[props.variant] : variants['default']} ${props.size ? sizes[props.size] : sizes['base']}`}></button>)
+  return (
+    <button
+      {...props}
+      className={`${variant ? variants[variant] : variants['default']} ${size ? sizes[size] : sizes['base']}`}
+    >
+      {children}
+    </button>
+  )
 }
---
+++ components/providers.js
@@ -0,0 +1,11 @@
+'use client'
+
+import { CookiesProvider } from 'react-cookie'
+
+export default function Providers({ children }) {
+  return (
+    <CookiesProvider>
+      {children}
+    </CookiesProvider>
+  )
+} 

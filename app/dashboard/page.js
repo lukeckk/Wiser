96107c71f4caf4f7dec2a8cd38c23744a649f6eb
Luke Cheng <Lukeecheng@gmail.com>
2025-04-02 11:29:27 -0700

    adding loading fallback for each trend

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -2,21 +2,22 @@ import TransactionList from "@/components/transaction-list"
 import { Suspense } from "react"
 import TransactionListFallback from "@/components/transaction-list-fallback"
 import TrendFetch from "@/components/trend-fetch"
+import TrendFallback from "@/components/trend-fallback"
 
 export default function Dashboard() {
   return (
     <>
       <section className="mb-8 grid grid-cols-2 lg:grid-cols-4 gap-8">
-        <Suspense>
+        <Suspense fallback={<TrendFallback />}>
           <TrendFetch type="Income" />
         </Suspense>
-        <Suspense>
+        <Suspense fallback={<TrendFallback />}>
           <TrendFetch type="Expense" />
         </Suspense>
-        <Suspense>
+        <Suspense fallback={<TrendFallback />}>
           <TrendFetch type="Saving" />
         </Suspense>
-        <Suspense>
+        <Suspense fallback={<TrendFallback />}>
           <TrendFetch type="Investment" />
         </Suspense>
       </section>

2ddd1315e4e2257c42a6978ba8ec0b5bd0991234
Luke Cheng <Lukeecheng@gmail.com>
2025-04-21 19:45:05 -0700

    configured formAction to store login state using useFormState hook

--- components/login-form.js
+++ components/login-form.js
@@ -3,12 +3,23 @@ import Button from "@/components/button";
 import Input from "@/components/input";
 import { login } from "@/lib/actions";
 import SubmitButton from "./submit-button";
+import { useFormState } from "react-dom"
+
+const initialState = {
+  message: '',
+  error: false
+}
 
 export default function LoginForm() {
-  return <form action={login} className="space-y-2">
+  const [state, formAction] = useFormState(login, initialState)
+  return <form action={formAction} className="space-y-2">
     <Input type="email" placeholder="name@example.com"
       name="email" required />
     <SubmitButton type="submit" size="sm" className="w-full">
       Sign in with email
-    </SubmitButton>  </form>
+    </SubmitButton>
+    <p className={`${state?.error ? 'text-red-500' : 'text-green-500'} text-sm text-center`}>
+      {state?.message}
+    </p>
+  </form>
 }

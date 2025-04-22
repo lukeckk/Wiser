760370dfcdabb0ea0e1b3ae54f9d42ba4fc7744a
Luke Cheng <Lukeecheng@gmail.com>
2025-04-22 14:44:53 -0700

    configured login with otp by updating path in email templates on subase

--- app/(auth)/auth/confirm/route.js
+++ app/(auth)/auth/confirm/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import { createClient } from '@/utils/supabase/server'
+import { createClient } from '@/lib/supabase/server'
 
 // verifies the OTP and dirent use to the next page '/' if successful, else redirects to '/error'
 

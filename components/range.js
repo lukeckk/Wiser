d3601ccb92bd77282d529feb33d555921e1d4aa8
Luke Cheng <Lukeecheng@gmail.com>
2025-05-02 22:07:32 -0700

    configured setting form to set fullName and default date range in user meta data in supabase

--- components/range.js
+++ components/range.js
@@ -1,5 +1,5 @@
 'use client'
-import Select from "@/components/select";
+import DateRangeSelect from "./data-range-select";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function Range() {
@@ -19,10 +19,5 @@ export default function Range() {
     replace(`${pathname}?${params.toString()}`)
   }
 
-  return <Select value={range} onChange={handleChange}>
-    <option value="last24hours">Last 24 hours</option>
-    <option value="last7days">Last 7 days</option>
-    <option value="last30days">Last 30 days</option>
-    <option value="last12months">Last 12 months</option>
-  </Select>
+  return <DateRangeSelect value={range} onChange={handleChange} />
 }

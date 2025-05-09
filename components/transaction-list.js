74c8988d656c6f717e081608c47c5e8d68f7da2a
Luke Cheng <Lukeecheng@gmail.com>
2025-05-09 08:42:44 -0700

    removed 'load more' button in the dashboard

--- components/transaction-list.js
+++ components/transaction-list.js
@@ -13,20 +13,20 @@ export default function TransactionList({ range, initialTransactions }) {
   const [loading, setLoading] = useState(false)
   const grouped = groupAndSumTransactionsByDate(transactions)
 
-  const handleClick = async () => {
-    setLoading(true)
-    let nextTransactions = null
-    try {
-      nextTransactions = await fetchTransactions(range, transactions.length, 10)
-      setButtonHidden(nextTransactions.length === 0)
-      setTransactions(prevTransactions => [
-        ...prevTransactions,
-        ...nextTransactions
-      ])
-    } finally {
-      setLoading(false)
-    }
-  }
+  // const handleClick = async () => {
+  //   setLoading(true)
+  //   let nextTransactions = null
+  //   try {
+  //     nextTransactions = await fetchTransactions(range, transactions.length, 2)
+  //     setButtonHidden(nextTransactions.length === 0)
+  //     setTransactions(prevTransactions => [
+  //       ...prevTransactions,
+  //       ...nextTransactions
+  //     ])
+  //   } finally {
+  //     setLoading(false)
+  //   }
+  // }
 
   // update the local React state by filtering out the deleted transaction:
   const handleRemoved = (id) => () => {
@@ -48,14 +48,14 @@ export default function TransactionList({ range, initialTransactions }) {
           </div>
         )}
       {transactions.length === 0 && <div className="text-center text-gray-400 dark:text-gray-500">No transactions found</div>}
-      {!buttonHidden && <div className="flex justify-center">
+      {/* {!buttonHidden && <div className="flex justify-center">
         <Button variant="ghost" onClick={handleClick} disabled={loading}>
           <div className="flex items-center space-x-1">
             {loading && <Loader className="animate-spin" />}
             <div>Load More</div>
           </div>
         </Button>
-      </div>}
+      </div>} */}
     </div>
   )
 }
